1426e2fb91037976c941a1dc7e2f4ae04823d246
Megha Rastogi <compchap.megha@gmail.com>
2016-10-02 16:28:36 -0700

    Learning Swift - Variables and Constants

---
+++ Learning-Swift/Learning Swift - Variables and Constants.playground/Contents.swift
@@ -0,0 +1,27 @@
+/* Copyright (C) {2016} {Megha Rastogi}
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+*/
+
+
+import UIKit
+
+//Variables and Constants
+
+//Constants
+
+//Remove the comments
+//let firstName : String = "John"
+//firstName = "Dave"
+
+//Variables
+var firstName : String = "John"
+print(firstName)
+
+//Changing value of variable
+firstName = "Dave"
+
+//Another way of assigning values without using types
+var lastName = "Sanders"
+print(lastName)
+
+
