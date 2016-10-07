e4d5344a184b2cac008aace6d2e30a8318d43c3a
Megha Rastogi <compchap.megha@gmail.com>
2016-10-07 15:38:19 -0700

    Types

---
+++ Learning-Swift/LearningSwift - Types.playground/Contents.swift
@@ -0,0 +1,69 @@
+/* Copyright (C) {2016} {Megha Rastogi}
+ You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+
+import UIKit
+
+/* Data Types
+    1. String
+    2. Character
+    3. Integer
+    4. Double
+    5. Floating
+    6. Boolean
+    7. Optional
+ */
+
+//String
+
+var firstName : String = "John"
+print (firstName)
+
+//Character
+
+var alpha : Character = "i"
+print (alpha)
+
+//Integer - Int and UInt
+
+var age : Int = 89
+print (age)
+
+var temp : Int = -78
+print (temp)
+
+//Remove the next comment and see the error
+//var numberOfOranges : UInt = -100
+//print(numberOfOranges)
+
+//Float
+
+var weight : Float = 78.9
+print(weight)
+
+//Double
+var height : Double = 9.67786
+print(height)
+
+//Boolean
+var light : Bool = true
+print(light)
+
+
+//Optional - We will see it in next video
+
+//Type Safety
+
+var dob : Int = 14
+//Remove the next comment and see the error
+//dob = "Hello My date of birth is "
+
+//Type Inference
+
+var score = 14
+print (score)
+
+
+
+
