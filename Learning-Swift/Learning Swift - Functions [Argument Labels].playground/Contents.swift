ee60f8ac21984db6d88f1ca53eb1962e03b30339
Megha Rastogi <compchap.megha@gmail.com>
2017-03-31 17:28:23 -0700

    argument labels

---
+++ Learning-Swift/Learning Swift - Functions [Argument Labels].playground/Contents.swift
@@ -0,0 +1,30 @@
+/* Copyright (C) {2016} {Megha Rastogi}
+ You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+
+/*
+ func functionName (argumentLabel paramName: paramType  ) -> returnType{
+ 
+ //block of code
+ return object
+ 
+ }
+ */
+
+//Example
+//Factorial of a Number
+
+func factorial (of number: Int) -> Int{
+    var factorial = 1
+    var temp = number
+    while(temp>1){
+        factorial =  factorial * temp
+        temp = temp - 1
+    }
+    return factorial
+}
+
+//functionName(paramName: paramValue)
+
+print("Factorial is \(factorial(of: 7))")
