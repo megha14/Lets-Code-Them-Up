4fae9b3d600cae0e500c11c4cad1cf7af267be02
Megha Rastogi <compchap.megha@gmail.com>
2020-06-06 15:49:25 -0700

    Update MaxSubArray.java

--- Algorithms/Arrays/MaxSubArray.java
+++ Algorithms/Arrays/MaxSubArray.java
@@ -14,13 +14,25 @@ class MaxSubArray {
      * @return integer, maxSum
      */
     public int maxSubArray(int[] nums) {
+        
+        // Initalize maxSum and sum
         int maxSum = Integer.MIN_VALUE, sum = 0;
+        
+        // Loop through the array
         for(int i = 0; i < nums.length; i++){
+            
+            // add current value to sum
             sum+=nums[i];
+            
+            // update sum if it becomes less than current value
             if(sum < nums[i])
                 sum = nums[i];
+            
+            // update the max sum
             maxSum = Math.max(maxSum, sum);
         }
+        
+        //return the max sum
         return maxSum;
     }
 }
