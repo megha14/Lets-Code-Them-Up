d1f36360f18a7f08cdeb0499eb05ef6ffee931d9
Megha Rastogi <compchap.megha@gmail.com>
2020-07-13 05:08:48 -0700

    Create SingleNumber.java

---
+++ Algorithms/Arrays/SingleNumber.java
@@ -0,0 +1,27 @@
+/**
+ * MaxSubArray.java
+ * Purpose: Find maximum sum contiguous subarray 
+ *
+ * @author Megha Rastogi
+ * @version 1.0 07/13/2020
+ */
+class SingleNumber {
+
+    /**
+     * Find maximum sum contiguous subarray 
+     *
+     * @param Array, nums
+     * @return integer, ans
+     */
+    public int singleNumber(int[] nums) {
+       
+        int ans = 0;
+        
+        // looping through the array and using XOR
+        // as a ^ a is 0 and 0 ^ b is b
+        for(int i = 0; i < nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
